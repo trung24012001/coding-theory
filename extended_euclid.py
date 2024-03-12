47d66c4a19dde02a4ad10582ee4f6604ccd59cf9
Vdtrung_ <hellovdtvip@gmail.com>
2024-03-12 13:53:09 +0700

    extended euclid algorithm

---
+++ extended_euclid.py
@@ -0,0 +1,22 @@
+def extended_euclid(a, b):
+    if b == 0:
+        return (a, 1, 0)
+
+    x2, x1 = 1, 0
+    y2, y1 = 0, 1
+    while b > 0:
+        q = a // b
+        r = a % b
+        x = x2 - q * x1
+        y = y2 - q * y1
+
+        a, b = b, r
+        x2, x1 = x1, x
+        y2, y1 = y1, y
+
+    d, x, y = (a, x2, y2)
+    return (d, x, y)
+
+
+a, b = 7, 13
+print(extended_euclid(a, b))
--- inv_coding.py
+++ inv_coding.py
@@ -1,14 +1,28 @@
-n = 26
+n = 9987
+x = 1844
+
+
+# def gcd(a, b):
+#     if b != 0:
+#         return a
+#     return gcd(b, a % b)
+
 
 def gcd(a, b):
-  if not b:
+    while b != 0:
+        r = a % b
+        a = b
+        b = r
     return a
-  return gcd(b, a%b)
+
 
 for i in range(1, n):
-  if gcd(i, n) != 1:
-    continue
-  for j in range(1, n):
-    if i * j % n == 1:
-      print(i, j)
-      break
+    if gcd(i, n) != 1:
+        continue
+    for j in range(1, n):
+        if i * j % n == 1:
+            if x == None:
+                print(i, j)
+            elif x == i:
+                print(i, j)
+            break
